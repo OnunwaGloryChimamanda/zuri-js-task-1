ca2bbd4a1851ede7a5a139f5a452331a3b5451b8
Onunwa Glory Chimamanda <62344621+OnunwaGloryChimamanda@users.noreply.github.com>
2022-06-28 22:13:04 +0100

    update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById ("name").innerHTML = "Name: Onunwa Glory .C."
+document.getElementById("height").innerHTML= "Height:12 feet"
+document.getElementById("country").innerHTML= "Country: Nigeria"
+
